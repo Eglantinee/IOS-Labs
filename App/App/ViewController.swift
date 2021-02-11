faf0b02a65a38b3920df5cd0bda21f5ad7974ee9
Ivan <vanya@iMac-Ivan.local>
2021-02-11 11:30:19 -0800

    initial project

---
+++ App/App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Ivan on 11.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
